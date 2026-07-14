80b05339b644402578b3565d3d72fd5847f1a52a
Marcello Barros <barroscello123@gmail.com>
2026-07-13 22:07:51 -0300

    Subindo a classe de conexão com a API do Google Books.

---
+++ src/consumindo_apis/desafiosModulo01/googleBooks/GoogleBooksClient.java
@@ -0,0 +1,28 @@
+package consumindo_apis.desafiosModulo01.googleBooks;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class GoogleBooksClient {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o título do livro para a busca: ");
+        var tituloLivro = leitura.nextLine();
+
+        String chave = "AIzaSyCGA_JhC9jgaCYEv-7cfrhXbZN1218AhPw";
+        String endereco = "https://www.googleapis.com/books/v1/volumes?q=" + URLEncoder.encode(tituloLivro, StandardCharsets.UTF_8) + "&key=" + chave;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
